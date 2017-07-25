e64e12a61f23bfa801c247f05adaa43386ec69d8
okutani-t <okutani.nt@gmail.com>
2017-07-25 17:14:17 +0900

    image plugin導入

--- app/javascript/packs/draft.jsx
+++ app/javascript/packs/draft.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Editor, EditorState, convertFromHTML, ContentState, RichUtils} from 'draft-js';
+import Editor from 'draft-js-plugins-editor';
+import createImagePlugin from 'draft-js-image-plugin';
+import {CompositeDecorator, ContentBlock, ContentState, EditorState, convertFromHTML, RichUtils} from 'draft-js';
 import {stateToHTML} from 'draft-js-export-html';
+
+import 'draft-js-image-plugin/lib/plugin.css';
+
+const imagePlugin = createImagePlugin();
+
+const plugins = [
+  imagePlugin,
+];
 
 class MyEditor extends React.Component {
   constructor(props) {
     super(props);
-    let defaultMarkup = document.getElementById('post_body').value;
+
+    //let defaultMarkup = document.getElementById('post_body').value;
+    let defaultMarkup =
+      '<b>Bold text</b>, <i>Italic text</i><br/ ><br />' +
+      '<a href="http://www.facebook.com">Example link</a><br /><br/ >' +
+      '<img src="https://assets-cdn.github.com/images/modules/open_graph/github-octocat.png" height="112" width="200" />';
     let blocksFromHTML = convertFromHTML(defaultMarkup);
     let state = ContentState.createFromBlockArray(
       blocksFromHTML.contentBlocks,
       blocksFromHTML.entityMap
     );
-    this.state = {editorState: EditorState.createWithContent(state)};
+    let initial = EditorState.createWithContent(state, new CompositeDecorator(decorators))
+    this.state = {editorState: initial};
     this.onChange = (editorState) => this.setState({editorState});
     this.handleKeyCommand = this.handleKeyCommand.bind(this);
   }
@@ -57,7 +73,7 @@ class MyEditor extends React.Component {
 
   render() {
     return (
-      <div className='editor'>
+      <div style={styles.root} className='editor'>
         <button type='button' onClick={this._onBoldClick.bind(this)}>Bold</button>
         <button type='button' onClick={this._onItalicClick.bind(this)}>Italic</button>
         <button type='button' onClick={this._onUnderlineClick.bind(this)}>Underline</button>
@@ -66,15 +82,83 @@ class MyEditor extends React.Component {
         <button type='button' onClick={this._onHeaderThreeClick.bind(this)}>H3</button>
         <button type='button' onClick={this._onListClick.bind(this)}>List</button>
         <button type='button' onClick={this._onBlockquoteClick.bind(this)}>Blockquote</button>
-        <Editor
-          editorState={this.state.editorState}
-          handleKeyCommand={this.handleKeyCommand}
-          onChange={this.handleChange.bind(this)}
-        />
+        <div style={styles.editor}>
+          <Editor
+            editorState={this.state.editorState}
+            handleKeyCommand={this.handleKeyCommand}
+            onChange={this.handleChange.bind(this)}
+            plugins={plugins}
+            decorators={decorators}
+          />
+        </div>
       </div>
     );
   }
 }
+
+function findLinkEntities(contentBlock, callback, contentState) {
+  contentBlock.findEntityRanges(
+    (character) => {
+      const entityKey = character.getEntity();
+      return (
+        entityKey !== null &&
+        contentState.getEntity(entityKey).getType() === 'LINK'
+      );
+    },
+    callback
+  );
+}
+
+const Link = (props) => {
+  const {url} = props.contentState.getEntity(props.entityKey).getData();
+  return (
+    <a href={url}>{props.children}</a>
+  );
+};
+
+function findImageEntities(contentBlock, callback, contentState) {
+  contentBlock.findEntityRanges(
+    (character) => {
+      const entityKey = character.getEntity();
+      return (
+        entityKey !== null &&
+        contentState.getEntity(entityKey).getType() === 'IMAGE'
+      );
+    },
+    callback
+  );
+}
+
+const Image = (props) => {
+  const {
+    height,
+    src,
+    width,
+  } = props.contentState.getEntity(props.entityKey).getData();
+
+  return (
+    <img src={src} height={height} width={width} />
+  );
+};
+
+const decorators = [
+  { strategy: findImageEntities, component: Image },
+  { strategy: findLinkEntities, component: Link },
+];
+
+const styles = {
+  root: {
+    fontFamily: '\'Helvetica\', sans-serif',
+    padding: 20,
+    width: 600,
+  },
+  editor: {
+    border: '1px solid #ccc',
+    cursor: 'text',
+    minHeight: 80,
+    padding: 10,
+  },
+};
 
 ReactDOM.render(
   <MyEditor />,
