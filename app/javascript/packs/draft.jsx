09653e502c516c918fe906fcf22891ddbf5639ba
okutani-t <okutani.nt@gmail.com>
2017-07-25 09:09:02 +0900

    装飾ボタン追加

--- app/javascript/packs/draft.jsx
+++ app/javascript/packs/draft.jsx
@@ -28,11 +28,39 @@ class MyEditor extends React.Component {
   _onBoldClick() {
     this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
   }
+  _onItalicClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'));
+  }
+  _onUnderlineClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
+  }
+  _onStrikethroughClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'STRIKETHROUGH'));
+  }
+  _onHeaderTwoClick() {
+    this.onChange(RichUtils.toggleBlockType(this.state.editorState, 'header-two'));
+  }
+  _onHeaderThreeClick() {
+    this.onChange(RichUtils.toggleBlockType(this.state.editorState, 'header-three'));
+  }
+  _onListClick() {
+    this.onChange(RichUtils.toggleBlockType(this.state.editorState, 'unordered-list-item'));
+  }
+  _onBlockquoteClick() {
+    this.onChange(RichUtils.toggleBlockType(this.state.editorState, 'blockquote'));
+  }
 
   render() {
     return (
       <div className='editor'>
         <button type='button' onClick={this._onBoldClick.bind(this)}>Bold</button>
+        <button type='button' onClick={this._onItalicClick.bind(this)}>Italic</button>
+        <button type='button' onClick={this._onUnderlineClick.bind(this)}>Underline</button>
+        <button type='button' onClick={this._onStrikethroughClick.bind(this)}>Delete</button>
+        <button type='button' onClick={this._onHeaderTwoClick.bind(this)}>H2</button>
+        <button type='button' onClick={this._onHeaderThreeClick.bind(this)}>H3</button>
+        <button type='button' onClick={this._onListClick.bind(this)}>List</button>
+        <button type='button' onClick={this._onBlockquoteClick.bind(this)}>Blockquote</button>
         <Editor
           editorState={this.state.editorState}
           handleKeyCommand={this.handleKeyCommand}
